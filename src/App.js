539c12f962be5a8b9210567cb1c8a7fb935109d5
Kundan Bhasin <kundanbhasin11062000@gmail.com>
2020-11-07 12:29:14 +0530

    created Shop page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import Header from './layouts/Header';
 import { Route } from 'react-router-dom';
 import AboutUs from './pages/AboutUs';
+import Shop from './pages/Shop';
 import Footer from './layouts/Footer';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Header />
       <main id="main">
         <Route path="/" exact component={AboutUs} />
+        <Route path="/shop" component={Shop} />
       </main>
       <Footer />
     </>
